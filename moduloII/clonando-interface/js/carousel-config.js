8afa34ec799bc23891a5a28cfd095102ceb49bd5
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-04-24 13:50:56 -0300

    ajustando, e correção do aquivo .js, do owl carrosel

--- moduloII/clonando-interface/js/carousel-config.js
+++ moduloII/clonando-interface/js/carousel-config.js
@@ -1,10 +1,13 @@
 
-//Section Destaque
+//Section Destaque para owl-carrosel classico
+/*
 $('.owl-carousel').owlCarousel({
     stagePadding: 50,
     loop: true,
     margin: 10,
     responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 1700,
     slideTransition: 'linear',
     responsive: {
         0: {
@@ -27,30 +30,58 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+*/
 
 //Section Promoção
-$('#nonloop .owl-carousel').owlCarousel({
-    center: true,
-    items: 2,
-    loop: false,
-    margin: 10,
-    autoplay: true,
-    autoplayTimeout: 1700,
-    responsive: {
+jQuery(document).ready(function($) {
+    $('.loop').owlCarousel({
+        stagePadding: 50,
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 1700,
+        slideTransition: 'linear',
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            425: {
+                items: 2,
+                nav: false
+            },
+            768: {
+                items: 4,
+                nav: false
+            },
+            1024: {
+                items: 6,
+                nav: false
+            }
+    
+        }
+    });
+    $('.nonloop').owlCarousel({
+      center: true,
+      loop: false,
+      margin: 10,
+      responsiveClass: true,
+      responsive: {
         0: {
             items: 1,
             nav: false
         },
-        425: {
+        768: {
             items: 2,
             nav: false
         },
         1024: {
             items: 3,
-            nav: true
+            nav: false
         }
-    }
-});
-
+      }
+    });
+  });
 
 
