88bcd097a1316063a8054adab3e65c8758ab093a
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-04-23 21:05:54 -0300

    Arrumando o css da pagina, iniciando a config do owlCarrosel

---
+++ moduloII/clonando-interface/js/carousel-config.js
@@ -0,0 +1,56 @@
+
+//Section Destaque
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    slideTransition: 'linear',
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        425: {
+            items: 2,
+            nav: false
+        },
+        768: {
+            items: 4,
+            nav: false
+        },
+        1024: {
+            items: 6,
+            nav: false
+        }
+
+    }
+})
+
+
+//Section Promoção
+$('.nonloop').owlCarousel({
+    center: true,
+    items: 2,
+    loop: false,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 1700,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        425: {
+            items: 2,
+            nav: false
+        },
+        1024: {
+            items: 3,
+            nav: true
+        }
+    }
+});
+
+
+
